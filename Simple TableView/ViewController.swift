f164f6c6672e7110a49a60ed0a84e20d7bf6f458
D7703_23 <d7703_23@D7703-23ui-iMac.local>
2018-05-03 16:06:25 +0900

    Table View

--- Simple TableView/ViewController.swift
+++ Simple TableView/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var animals = ["Cat", "Tiger", "Lion"]
+    var country = ["Korea", "China", "Africa"]
+    var ani = ["1.jpg", "2.jpg", "3.jpg"]
+    
+    // row의 수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
+    //cell의 메모리 할당
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // cell의 재사용
+        let identifier = "RE"
+        // cell을 하나 메모리 할당하고 재사용을 위해 저장("RE")
+        let cell  = myTableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        
+        //  배열에 있는 데이터를 cell에 넣기
+        cell.textLabel?.text = animals[indexPath.row]
+        // RE에서 subtitle or right detail
+        cell.detailTextLabel?.text = country[indexPath.row]
+        
+        // 이미지넣기
+        let myImage = UIImage(named: ani[indexPath.row])
+        cell.imageView?.image = myImage
+        
+        return cell
+    }
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myTableView.delegate = self
+        myTableView.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
